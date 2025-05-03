b2754fbc2761d17e7f95795d396bc1371b2ee046
ChristopherL <anbchris@gmail.com>
2025-05-02 21:07:01 -0400

    start setting up a sign in component as well as a firebase authentication and database

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,64 @@
+
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider
+} from "firebase/auth";
+import { 
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    getDocs,
+    addDoc,
+    collection, 
+
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAiLk1RgtS30fCXyIXjnf81hwsZ9wCgSU",
+  authDomain: "cloud-apparel-db.firebaseapp.com",
+  projectId: "cloud-apparel-db",
+  storageBucket: "cloud-apparel-db.firebasestorage.app",
+  messagingSenderId: "510199093306",
+  appId: "1:510199093306:web:f118d842b9b94b1b667d03"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+
+export const auth = getAuth(firebaseApp);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore(firebaseApp);
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log('user doc ref: ', userDocRef);
+    const userSnapshot = await getDoc(userDocRef)
+    console.log('user snapshot: ', userSnapshot);
+    console.log('user snapshot exists?: ', userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('failed to set doc', error.message);
+        }
+    }
+
+    return userDocRef;
+}
