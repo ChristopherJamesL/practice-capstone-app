fba05a699313f1bd6356bf5afe3d526b9b2eca91
ChristopherL <anbchris@gmail.com>
2025-05-18 19:32:09 -0400

    added reselect to memoize category state to prevent unecessary re-renders

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,17 @@
+import { createSelector } from "reselect";
 
-export const selectCategoriesMap = (state) => {
-    return state.categories.categories.reduce((acc, category) => {
-            const { title, items } = category;
-            acc[title.toLocaleLowerCase()] = items;
-            return acc;
-        }, {})
-};
+const selectCategoryReducer = (state) => state.categories;
+
+const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => categories.reduce((acc, category) => {
+        const { title, items } = category;
+        acc[title.toLocaleLowerCase()] = items;
+        return acc;
+    }, {})
+);
